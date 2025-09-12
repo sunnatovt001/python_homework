475c81a75bd85bb1f09a2c38b740b34e9042e457
sunnatovt001 <sunnatovtolibjon7@gmail.com>
2025-09-12 19:39:04 +0500

    Create hw-1.py

---
+++ lesson-1/homework/hw-1.py
@@ -0,0 +1,38 @@
+#GitHub Homework lesson 1
+#9/12/2025
+#By: Tolibjon
+
+
+
+
+# 1.Given a side of square. Find its perimeter and area.
+a=2
+b=3
+P=4*a
+S=a*a
+print("Perimeter of square is:",P)
+print("Area of square is:",S)
+
+# 2.Given diameter of circle. Find its length.
+d=4
+L=3.14*d
+print("Length of circle is:",L)
+
+# 3.Given two numbers a and b. Find their mean.
+a=5
+b=7
+mean=(a+b)/2
+print("Mean of two numbers is:",mean)
+
+# 4.Given two numbers a and b. Find their sum, product and square of each number.
+a=3
+b=4
+sum=a+b
+product=a*b
+square_a=a**2
+square_b=b**2
+print("Sum of two numbers is:",sum)
+print("Product of two numbers is:",product)
+print("Square of a is:",square_a)
+print("Square of b is:",square_b)
+
